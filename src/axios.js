60a48dac40c1d52d3da6acebe9964bb27f62c385
daggubatibhargavi <daggubatibhargavi684@gmail.com>
2025-05-29 15:06:39 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,28 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: "https://tracks-backend-uorz.onrender.com/api/v1",
+//    // baseURL: "http://localhost:5000/api/v1",
+// });
+
+// // Automatically attach token to every request
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default axiosInstance;
+
+// src/axios.js
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://tracks-backend-uorz.onrender.com/api/v1",
-   // baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://tracks-backend-uorz.onrender.com/api/v1", // or your localhost
 });
 
-// Automatically attach token to every request
 axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
@@ -15,3 +32,4 @@ axiosInstance.interceptors.request.use((config) => {
 });
 
 export default axiosInstance;
+
